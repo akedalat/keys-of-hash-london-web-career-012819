6e459512c4d2f4d646dd5352cc89e1125179e97b
Kian Edalat <akedalat@gmail.com>
2019-01-25 12:33:21 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -6,7 +6,7 @@ class Hash
         if arguments.include?(v)
           return output_array << k
         else
-           return output_array << v
+           return output_array << k
         end
     end
   end
