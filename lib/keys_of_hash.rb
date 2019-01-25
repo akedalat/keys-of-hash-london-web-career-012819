3745f848371a989ba7fa938be2bd35a6f39945b9
Kian Edalat <akedalat@gmail.com>
2019-01-25 12:27:48 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,11 @@ require "pry"
 class Hash
   def keys_of(*arguments)
     arguments.collect do |k, v|
-if *arguments.include?(v)
-  return k
-else
-  return v
+        if *arguments.include?(v)
+          return k
+        else
+          return v
+        end
     end
   end
 end
