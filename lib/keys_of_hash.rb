cfac48746a680d710c3882175b22c6a7ad806071
Kian Edalat <akedalat@gmail.com>
2019-01-25 12:49:18 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    output_array = []
+    map do |k, v|
+      if arguments.include?(v)
+        output_array << k
+      end
+    end
+    return output_array
   end
 end
